7311f102b1a4d6a3538f2c3458423f0aee386fb2
Mimi Ke <mimi.g.ke@gmail.com>
2021-01-14 15:54:32 -0600

    second phase

--- connect-four.js
+++ connect-four.js
@@ -1,27 +1,42 @@
 import Game from './game.js';
+
 let game = undefined;
+
 function updateUI () {
     let boardHolder = document.getElementById('board-holder')
-    if(game === undefined){
+    if(game === undefined){ // does not exist
         boardHolder.classList.add('is-invisible')
     } else {
         let gameName = document.getElementById('game-name')
         gameName.innerHTML = game.getName()
         boardHolder.classList.remove('is-invisible')
+        if (game.current === 1){
+            clickTarget.classList.add("black") //css class
+            clickTarget.classList.remove("red")
+        } else { // if it's 2
+            clickTarget.classList.add("red") // switching the colors 
+            clickTarget.classList.remove("black")
+        }
     }
 }
 
 window.addEventListener("DOMContentLoaded", event => {
+    let clickTarget = document.getElementById("click-targets")
     let playerName1 = document.getElementById("player-1-name")
     let playerName2 = document.getElementById("player-2-name")
     let newGame = document.getElementById("new-game")
     let formHolder = document.getElementById("form-holder")
+
     formHolder.addEventListener('keyup', event => {
         if ((playerName1.value) && (playerName2.value)){
             newGame.disabled = false
         } else {
             newGame.disabled = true
         }
+        updateUI()
+    })
+    clickTarget.addEventListener("click", event => {
+        game.playInColumn() // updates the current players turn ++ --
         updateUI()
     })
 })
--- game.js
+++ game.js
@@ -2,8 +2,16 @@ class Game {
     constructor(name1, name2) {
         this.name1 = name1;
         this.name2 = name2;
+        this.current = 1;
     }
     getName(){
         return `player 1 ${this.name1} vs. player 2 ${this.name2}`
+    }
+    playInColumn(){
+        if(this.current === 1){
+            this.current++
+        } else {
+            this.current--
+        }
     }
 }
